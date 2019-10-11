aed65d97badacd9a01ab349ffffa3b5b910be15d
OleksiiFushtei <oleksiifushtei@gmail.com>
2019-10-11 19:23:45 +0200

    login layout update
    Custom button and new layout for login page

--- app/src/main/java/com/example/final_project/SignInActivity.kt
+++ app/src/main/java/com/example/final_project/SignInActivity.kt
@@ -2,11 +2,14 @@ package com.example.final_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
     }
 }
